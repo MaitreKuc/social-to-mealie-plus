381b55bf03ba51d003f1de64d3851b2382b5bca7
gerardPolloRebozado <gerardit4@gmail.com>
2025-02-18 19:36:21 +0100

    feat: :sparkles: Improved error reporting by adding a card that show in which step failed

--- src/app/api/get-url/route.ts
+++ src/app/api/get-url/route.ts
@@ -1,39 +1,60 @@
-import { NextResponse } from 'next/server';
-import type { recipeInfo } from '@//lib/types';
-import { getInstagram } from '@//social-networks/instagram';
 import { getRecipe, postRecipe } from '@//lib/mealie';
+import type { progressType, recipeInfo, socialMediaResult } from '@//lib/types';
+import { getInstagram } from '@//social-networks/instagram';
+import { getTranscription } from '@/lib/ai';
 import { getTiktok } from '@/social-networks/tiktok';
+import { NextResponse } from 'next/server';
 
 interface RequestBody {
   url: string;
 }
 
-async function processRecipe({ url }: RequestBody) {
-  let data: recipeInfo;
+let progress: progressType = {
+  videoDownloaded: null,
+  audioTranscribed: null,
+  recipeCreated: null,
+};
 
-  if (!url) {
-    return NextResponse.json({ error: 'URL is required' }, { status: 400 });
+async function processRecipe({ url }: RequestBody) {
+  try {
+    let socialMediaResult: socialMediaResult;
+    progress = {
+      videoDownloaded: null,
+      audioTranscribed: null,
+      recipeCreated: null,
+    };
+
+    if (!url) {
+      return NextResponse.json({ error: 'URL is required' }, { status: 400 });
+    }
+    if (url.includes('instagram')) {
+      socialMediaResult = await getInstagram({ url });
+      progress.videoDownloaded = true;
+    } else if (url.includes('tiktok')) {
+      socialMediaResult = await getTiktok({ url });
+      progress.videoDownloaded = true;
+    } else {
+      progress.videoDownloaded = false;
+      return NextResponse.json({ error: 'Error downloading video', progress }, { status: 400 });
+    }
+    const transcription = await getTranscription(socialMediaResult.blob);
+    progress.audioTranscribed = true;
+    const data: recipeInfo = {
+      postURL: url,
+      description: socialMediaResult.description,
+      transcription,
+      thumbnail: socialMediaResult.thumbnail,
+    };
+    const mealieResponse = await postRecipe(data);
+    const createdRecipe = await getRecipe(await mealieResponse);
+    progress.recipeCreated = true;
+    return NextResponse.json(createdRecipe);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message, progress }, { status: 500 });
   }
-  if (url.includes('instagram')) {
-    data = await getInstagram({ url });
-  } else if (url.includes('tiktok')) {
-    data = await getTiktok({ url });
-  } else {
-    return NextResponse.json({ error: 'Invalid URL' }, { status: 400 });
-  }
-  const mealieResponse = await postRecipe(data);
-  const createdRecipe = await getRecipe(await mealieResponse);
-  return NextResponse.json(createdRecipe);
 }
 
 export async function POST(req: Request) {
-  try {
-    const body: RequestBody = await req.json();
-    return await processRecipe(body);
-  } catch (error: any) {
-    if (error?.message) {
-      return NextResponse.json({ error: `Invalid request ${error.message}` }, { status: 400 });
-    }
-    return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
-  }
+  const body: RequestBody = await req.json();
+  return await processRecipe(body);
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,5 +5,5 @@ export const env: envTypes = {
   OPENAI_API_KEY: process.env.OPENAI_API_KEY?.trim() as string,
   WHISPER_MODEL: process.env.WHISPER_MODEL?.trim() as string,
   MEALIE_URL: process.env.MEALIE_URL?.trim().replace(/\/+$/, '') as string,
-  MEALIE_API_KEY: process.env.MEALIE_API_KEY?.trim() as string,
+  MEALIE_API_KEY: process.env.MEALIE_API_KEY?.trim().replace(/\n/g, '') as string,
 };
