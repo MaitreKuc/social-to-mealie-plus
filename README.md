0f779872109339972bfd65253f6ab9898ca313f8
Gerard Du Pre <gerardit4@gmail.com>
2025-05-28 10:34:00 +0200

    feat: :sparkles: Moved from using snapsave and puppeteer to yt-dlp to improve compatibility along many websites and reduce docker size image

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Social media to Mealie
 
 Found any recipe on a social media and don't want to manually write it yourself? use our tool to import any video from them in Mealie.
-For now only works with Instagram,TikTok, YouTube, and Pinterest (without cover image) but will add more if you open an 
-issue. 
+The only tested social media are:
+- Instagram
+- TikTok
+- Facebook
+- YouTube Shorts
+- Pinterest
+But it may work with others as it used yt-dlp to download the video, if you find any issues using other websites, please open an issue.
 Currently, some post may give BAD_RECIPE error, with is a mealie issue parsing the recipe, I have still not found why 
 sometimes works and sometimes not.
 The prompt can be customized in the ENV, so if you find a better one, please open an issue or PR.
@@ -15,10 +20,6 @@ To deploy this just use the docker-compose.yml and populate all the environment 
 
 -   Import posts in Mealie with a link and the click of a button
 -   Use this [ios shortcut](https://www.icloud.com/shortcuts/a66a809029904151a39d8d3b98fecae4) so you just need to click share button
-
-## Roadmap
-
-You can check the roadmap at the [projects tab](https://github.com/users/GerardPolloRebozado/projects/8)
 
 ## Screenshot
 
--- src/lib/yt-dlp.ts
+++ src/lib/yt-dlp.ts
@@ -1,12 +1,13 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import type { socialMediaResult } from '@/lib/types';
 import YTDlpWrap from 'yt-dlp-wrap';
 
 const ytDlpPath = path.resolve('./yt-dlp');
 const outputDir = path.resolve('./temp');
 
 export async function ensureYtDlpBinary() {
-  const ytDlpVersion = process.env.YT_DLP_VERSION || '2025.03.31';
+  const ytDlpVersion = process.env.YT_DLP_VERSION || '2025.05.22';
   const exists = await fs
     .access(ytDlpPath)
     .then(() => true)
@@ -24,17 +25,26 @@ export async function ensureYtDlpBinary() {
   }
 }
 
-export async function downloadWithYtDlp(url: string, filename = 'video.mp4') {
+export async function downloadWithYtDlp(url: string) {
   await ensureYtDlpBinary();
   const ytDlpWrap = new YTDlpWrap(ytDlpPath);
-  const outputFile = path.join(outputDir, filename);
+  const outputFile = path.join(outputDir, 'audio.wav');
   await fs.mkdir(outputDir, { recursive: true });
   try {
-    await ytDlpWrap.execPromise([url, '-f', 'best', '-o', outputFile]);
+    await ytDlpWrap.execPromise([url, '-f', 'bestaudio', '-x', '--audio-format', 'wav', '-o', outputFile]);
     const metadata = await ytDlpWrap.getVideoInfo(url);
     const buffer = await fs.readFile(outputFile);
     return { buffer, metadata };
   } finally {
     await fs.unlink(outputFile).catch(() => {});
   }
+}
+
+export async function downloadMediaWithYtDlp(url: string): Promise<socialMediaResult> {
+  const { buffer, metadata } = await downloadWithYtDlp(url);
+  return {
+    blob: new Blob([buffer], { type: 'audio/mp3' }),
+    thumbnail: metadata.thumbnail,
+    description: metadata.description || 'No description found',
+  };
 }
